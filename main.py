81ce8cab1b428e76144738b9964857ff5720388b
rodakalex <ravengothic@mail.ru>
2023-08-05 02:15:26 +0300

    v 0.0.3 рефакторинг, куки, плюс исправление ошибок

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
+import time, pickle, os
 from urllib import parse
-from selenium.common import TimeoutException, NoSuchElementException
+
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -15,6 +16,7 @@ roles = {
     1: 'Event-manager',
     2: 'PR-manager'
 }
+BASE_URL = "https://hh.ru"
 
 
 class Main:
@@ -22,33 +24,36 @@ class Main:
     driver = webdriver.Chrome()
     driver.set_page_load_timeout(10)
 
-    def __init__(self, vacancy: str, role: int, salary: int = 0, only_with_salary: bool = False, schedules: list = None):
-        self.schedule = ''
-
-        for schedule in schedules:
-            self.schedule += f"schedule={schedule}&"
-
-        if not len(schedules):
-            self.schedule = self.schedule[0:len(self.schedule)]
-
-        self.login: str = config.login
-        self.password: str = config.password
-        self.vacancy: str = parse.quote_plus(vacancy)
-        self.salary: int = salary
+    def __init__(self, vacancy: str, professional_role: int, salary: int = 0, only_with_salary: bool = False,
+                 schedules: list = None, areas: list = None):
+        self.schedule = '&'.join(f"schedule={schedule}" for schedule in schedules) if schedules else ''
+        self.area = '&'.join(f"area={area}" for area in areas) if areas else ''
+        self.login = config.login
+        self.password = config.password
+        self.vacancy = parse.quote_plus(vacancy)
+        self.salary = salary
         self.only_with_salary = only_with_salary
-        self.role = role
-        
+        self.professional_role = professional_role
+
+    def open_cookie(self):
+        with open('cookie.pickle', 'rb') as file:
+            cookies = pickle.load(file)
+            self.open_page_without_js(BASE_URL)
+            for cookie in cookies:
+                self.driver.add_cookie(cookie)
+        return 'Ok'
+
     def auth(self) -> str:
         self.driver.set_page_load_timeout(5)
         try:
-            self.driver.get('https://hh.ru/account/login?backurl=%2F&hhtmFrom=main')
+            self.driver.get(f'{BASE_URL}/account/login?backurl=%2F&hhtmFrom=main')
         except TimeoutException:
             self.driver.execute_script("window.stop();")
-            login_by_password = self.driver.find_element(By.XPATH, "//button[@data-qa='expand-login-by-password']")
+            login_by_password = self.driver.find_element(By.CSS_SELECTOR, 'button[data-qa="expand-login-by-password"]')
             login_by_password.click()
-            username = self.driver.find_element(By.XPATH, "//input[@data-qa='login-input-username']")
+            username = self.driver.find_element(By.CSS_SELECTOR, "input[data-qa='login-input-username']")
             username.send_keys(self.login)
-            password = self.driver.find_element(By.XPATH, "//input[@data-qa='login-input-password']")
+            password = self.driver.find_element(By.CSS_SELECTOR, "input[data-qa='login-input-password']")
             password.send_keys(self.password)
             password.send_keys(Keys.ENTER)
 
@@ -59,16 +64,16 @@ class Main:
         self.driver.set_page_load_timeout(5)
 
         while True:
-            page_link = (f'https://hh.ru/search/vacancy?professional_role={self.role}&schedule=remote'
-                         f'&search_field=name&search_field=company_name&search_field=description'
-                         f'&enable_snippets=true&salary={self.salary}&only_with_salary='
-                         f'{self.only_with_salary}&text='
-                         f'{self.vacancy}&from=suggest_post&L_save_area=true&page={index}')
+            vacancy_search_url = (f'{BASE_URL}/search/vacancy?{self.area}&professional_role={self.professional_role}&'
+                                  f'{self.schedule}&search_field=name&search_field=company_name&'
+                                  f'search_field=description&enable_snippets=true&salary={self.salary}&'
+                                  f'only_with_salary={self.only_with_salary}&text='
+                                  f'{self.vacancy}&from=suggest_post&L_save_area=true&page={index}')
             try:
-                if self.driver.current_url != page_link:
-                    self.driver.get(page_link)
+                if self.driver.current_url != vacancy_search_url:
+                    self.driver.get(vacancy_search_url)
 
-                vacancy_list = self.driver.find_elements(By.XPATH, "//a[@data-qa='vacancy-serp__vacancy_response']")
+                vacancy_list = self.driver.find_elements(By.CSS_SELECTOR, "a[data-qa='vacancy-serp__vacancy_response']")
                 vacancy_count = len(vacancy_list)
 
                 for link in vacancy_list:
@@ -91,19 +96,25 @@ class Main:
                         self.driver.get(link)
 
                     try:
-                        xpath_letter_button = '//button[@data-qa="vacancy-response-letter-toggle"]'
-                        letter_button = self.driver.find_element(By.XPATH, xpath_letter_button)
+                        xpath_letter_button = 'button[data-qa="vacancy-response-letter-toggle"]'
+                        letter_button = self.driver.find_element(By.CSS_SELECTOR, xpath_letter_button)
                         letter_button.click()
                     except NoSuchElementException:
                         pass
 
-                    xpath_letter = "//textarea[@data-qa='vacancy-response-popup-form-letter-input']"
-                    letter = self.driver.find_element(By.XPATH, xpath_letter)
-                    letter.send_keys('Добрый день! Пожалуйста, рассмотрите мою кандидатуру')
+                    try:
+                        xpath_letter = "textarea[data-qa='vacancy-response-popup-form-letter-input']"
+                        letter = self.driver.find_element(By.CSS_SELECTOR, xpath_letter)
+                        letter.send_keys('Добрый день! Пожалуйста, рассмотрите мою кандидатуру')
+                    except NoSuchElementException:
+                        pass
 
-                    xpath_submit_button = '//button[@data-qa="vacancy-response-submit-popup"]'
-                    submit_button = self.driver.find_element(By.XPATH, xpath_submit_button)
-                    submit_button.click()
+                    try:
+                        xpath_submit_button = 'button[data-qa="vacancy-response-submit-popup"]'
+                        submit_button = self.driver.find_element(By.CSS_SELECTOR, xpath_submit_button)
+                        submit_button.click()
+                    except NoSuchElementException:
+                        pass
 
                     time.sleep(2)
 
@@ -113,16 +124,35 @@ class Main:
                 attempt += 1
         print('Отклики на все вакансии успешно оставлены!')
 
+    def save_cookie(self):
+        with open('cookie.pickle', 'wb') as cookie:
+            self.driver.set_page_load_timeout(100)
+            pickle.dump(self.driver.get_cookies(), cookie)
+            self.driver.set_page_load_timeout(10)
+
+    def open_page_without_js(self, url):
+        try:
+            self.driver.get(url)
+        except TimeoutException:
+            self.driver.execute_script("window.stop();")
+
     def __del__(self):
-        self.driver.close()
-        exit()
+        self.driver.quit()
 
 
 if __name__ == '__main__':
     # Пример использования
-    schedules = ['remote', 'fullDay']
-    m = Main(vacancy='1С', salary=300000, only_with_salary=True, role=96, schedules=schedules)
-    status_auth = m.auth()
+    schedules = []
+    m = Main(vacancy='1С', salary=300000, only_with_salary=True, professional_role=96, schedules=schedules, areas=['2019', '1'])
+    status_auth = None
+
+    if not os.path.exists('./cookie.pickle'):
+        status_auth = m.auth()
+        question_save_cookie = input('Сохранить логин и пароль? (y/n)\n')
+        if question_save_cookie == 'y':
+            m.save_cookie()
+    else:
+        status_auth = m.open_cookie()
     if status_auth == 'Ok':
         m.get_links()
         m.response()
