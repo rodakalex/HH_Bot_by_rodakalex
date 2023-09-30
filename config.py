fec03babcceef45f584df60a9622867b1c5ae48d
rodakalex <ravengothic@mail.ru>
2023-09-30 14:59:40 +0300

    v.0.0.4, поредачил файл конфига, добавил функционал задавания логина и пароля непосредственно в модуль файла, добавил файл auth в gitignore

--- config.py
+++ config.py
@@ -1,2 +1,24 @@
+import os
+
 login = ''
 password = ''
+
+
+def authenticate():
+    global login, password
+    import auth
+    login = auth.login
+    password = auth.password
+
+
+if os.path.exists('auth.py'):
+    authenticate()
+
+else:
+    with open('auth.py', 'w') as auth:
+        login = input('Введите email\n?> ')
+        password = input('Введите пароль\n?> ')
+        print('> ')
+        auth.write(f"login = '{login}'\npassword = '{password}'")
+
+    authenticate()
