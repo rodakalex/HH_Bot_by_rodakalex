e4739d99cb4dbef816c13496ca7be67b18f3c1b2
rodakalex <ravengothic@mail.ru>
2024-11-03 20:09:41 +0300

    Удалил лишнюю функцию

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-login = 'someemail@mail.ru'
-password = 'somepassword'
+login = 'ravengothic@mail.ru'
+password = 'A_Perfect_Sin4'
 job = 'Программист 1С'
 # Пример ссылки:
-# base_url = 'https://hh.ru/search/vacancy?hhtmFrom=main&hhtmFromLabel=vacancy_search_line&search_field=name&search_field=company_name&search_field=description&enable_snippets=false&L_save_area=true&excluded_text=%D0%90%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA&accept_temporary=true&text=%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%81%D1%82+1%D0%A1'
-base_url = ''
+base_url = 'https://hh.ru/search/vacancy?hhtmFrom=main&hhtmFromLabel=vacancy_search_line&search_field=name&enable_snippets=false&L_save_area=true&excluded_text=%D0%90%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%2CBitrix%2CPython&accept_temporary=true&text=%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%81%D1%82+1%D0%A1'
+# base_url = ''
